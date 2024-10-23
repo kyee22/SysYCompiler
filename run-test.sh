9e6bcda17065f6c6c652d042a00e3cc091f48c33
kyee22 <buaakyee22@163.com>
2024-10-23 11:17:27 +0800

    engine

--- run-test.sh
+++ run-test.sh
@@ -5,7 +5,8 @@ cd lab/Compiler
 
 # 设置类路径，包括src、lib目录中的JAR文件
 LIB_DIR="lib"
-CLASSPATH=".:src:lib/*"
+CLASSPATH=".:src:$(ls lib/*.jar | tr ' ' ':')"
+
 
 # 使用find命令来查找所有Java文件并编译它们
 find . -name "*.java" | xargs -n 1 javac -encoding UTF-8 -cp "$CLASSPATH" -d .
